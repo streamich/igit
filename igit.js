56969914c547e1e589e05d553a806530cddd83b6
streamich <streamich@gmail.com>
2020-07-17 19:36:42 +0200

    fix: 🐛 use correct dist folder

--- igit.js
+++ igit.js
@@ -5,10 +5,10 @@ const { join } = require('path');
 const { platform } = require('os');
 
 const os = platform();
-const cwd = process.cwd();
 const filename = (os === 'win32' ? 'windows' : os) + '_amd64' + (os === 'win32' ? '.exe' : '');
-const command = join(cwd, 'dist', filename);
+const command = join(__dirname, 'dist', filename);
 const [,, ...args] = process.argv;
+const cwd = process.cwd();
 
 spawn(command, args, {
   cwd,
