e31eb1b5421ec64afdcbae3b3046695cdc57119c
streamich <streamich@gmail.com>
2020-07-17 18:45:23 +0200

    feat: 🎸 add date to semantic branches

--- internal/cmdbranch/format.go
+++ internal/cmdbranch/format.go
@@ -1,9 +1,20 @@
 package cmdbranch
 
+import "time"
+
 func formatBranchName(state *branchState) string {
 	name := state.info.branchType
 
-	if name != "" {
+	if name != "" && state.settings.useTime {
+		name = name + "/"
+	}
+
+	if state.settings.useTime {
+		timeFormatted := time.Now().Format(time.RFC3339)[0:10]
+		name = name + timeFormatted
+	}
+
+	if name != "" && state.info.name != "" {
 		name = name + "/"
 	}
 
